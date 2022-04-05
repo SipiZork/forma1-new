d9e0530de6034c7d7a6a7a6eb30e55b652a5af75
Mark <sipizork@gmail.com>
2022-04-06 01:05:39 +0200

    fix bugs

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -23,20 +23,21 @@ const Admin = ({ loggedIn, user, races }) => {
   const addPoints = (e) => {
     e.preventDefault();
     const actualRace = races.find(race => race.active);
-    actualRace.votes.map(async (vote) => {
-      console.log(vote);
-      const userRef = doc(db, 'users', vote.user);
-      const raceRef = doc(db, 'races-datas/2022/races', actualRace.id);
-      const userSnap = await getDoc(userRef);
-      const userPoints = userSnap.data().points2022;
-      let newPoints = 0;
-      if(actualRace.winners.first === vote.first) { newPoints += 25}
-      if(actualRace.winners.second === vote.second) { newPoints += 18}
-      if(actualRace.winners.third === vote.third) { newPoints += 15}
-      if (actualRace.winners.fastestLap === vote.fastestLap) { newPoints += 1; }
-      updateDoc(userRef, { points2022: userPoints + newPoints, points2022before: userPoints });
-      updateDoc(raceRef, { end: true, active: false });
-    });
+    const raceRef = doc(db, 'races-datas/2022/races', actualRace.id);
+    if (actualRace.votes > 0) {
+      actualRace.votes.map(async (vote) => {
+        const userRef = doc(db, 'users', vote.user);
+        const userSnap = await getDoc(userRef);
+        const userPoints = userSnap.data().points2022;
+        let newPoints = 0;
+        if (actualRace.winners.first === vote.first) { newPoints += 25; }
+        if (actualRace.winners.second === vote.second) { newPoints += 18; }
+        if (actualRace.winners.third === vote.third) { newPoints += 15; }
+        if (actualRace.winners.fastestLap === vote.fastestLap) { newPoints += 1; }
+        updateDoc(userRef, { points2022: userPoints + newPoints, points2022before: userPoints });
+      });
+    }
+    updateDoc(raceRef, { end: true, active: false });
   }
 
   return (
